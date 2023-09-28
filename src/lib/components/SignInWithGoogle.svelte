3cd07a30984624822139f1d67173f5902d4eb04f
JP Carlin <jpcarlin@jps-macbook-air.mynetworksettings.com>
2023-09-27 23:57:31 -0400

    Added signup route with Kai Google and password signin incomplete

---
+++ src/lib/components/SignInWithGoogle.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { auth, user } from '$lib/firebase'
+  import { GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth'
+
+  async function signInWithGoogle() {
+    const provider = new GoogleAuthProvider()
+    const credential = await signInWithPopup(auth, provider)
+    console.log({ credential })
+    const idToken = await credential.user.getIdToken()
+    console.log({ idToken })
+
+    const res = await fetch('/api/signin', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'CSRF-Token': csrftoken // HANDLED by sveltekit automatically
+      },
+      body: JSON.stringify({ idToken }),
+    })
+  }
+  async function signOutSSR() {
+    const res = await fetch('/api/signin', { method: 'DELETE' })
+    await signOut(auth)
+  }
+</script>
+
+{#if $user}
+  <h2 class="card-title">Welcome, {$user.displayName}</h2>
+  <p class="text-center text-success">You are logged in</p>
+  <button class="btn btn-warning" on:click={signOutSSR}>Sign out</button>
+{:else}
+  <button class="btn btn-primary" on:click={signInWithGoogle}
+    >Sign in with Google</button
+  >
+{/if}
+
+<slot />
---
+++ src/lib/components/SignUpWithPassword.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth'
+  let email = ''
+  let password = ''
+  async function signUpWithPassword() {
+    const auth = getAuth()
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        // ..
+      })
+  }
+</script>
+
+<form class="flex flex-col max-w-screen-md w-full">
+  <label class="text text-left font-bold mt-2" for="email">Email</label>
+  <input
+    bind:value={email}
+    name="email"
+    class="input input-bordered w-full max-w-xs"
+    type="text"
+  />
+  <label class="text text-left font-bold mt-2" for="password">Password</label>
+  <input
+    bind:value={password}
+    name="password"
+    class="input input-bordered w-full max-w-xs"
+    type="text"
+  />
+  <!-- <button on:click={signUpWithPassword} type="submit">Submit</button> -->
+</form>
