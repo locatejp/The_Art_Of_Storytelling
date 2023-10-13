aa54f8de394bdd106d4e9c1ca37fc8501e3b1300
JP Carlin <jpcarlin@jps-macbook-air.mynetworksettings.com>
2023-10-12 23:50:43 -0400

    Starting a new story works but I need to figure out the admin storage issue

---
+++ src/hooks.server.ts
@@ -0,0 +1,14 @@
+import { adminAuth } from '$lib/admin'
+import type { Handle } from '@sveltejs/kit'
+
+export const handle = (async ({ event, resolve }) => {
+  const sessionCookie = event.cookies.get('__session')
+  try {
+    const decodedClaims = await adminAuth.verifySessionCookie(sessionCookie!)
+    event.locals.userId = decodedClaims.uid
+  } catch (error) {
+    event.locals.userId = null
+    return resolve(event)
+  }
+  return resolve(event)
+}) satisfies Handle
