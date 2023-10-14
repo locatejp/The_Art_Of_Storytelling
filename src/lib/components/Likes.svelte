46ddbe888983171f13c8f9ab3295b31bd8e9c12f
JP Carlin <jpcarlin@jps-macbook-air.mynetworksettings.com>
2023-10-14 11:50:46 -0400

    Got the like heart working

---
+++ src/lib/components/Likes.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  export let uid: string | undefined
+  export let likes: string[]
+  export let storyId: string
+  import { db } from '$lib/firebase'
+  import {
+    arrayRemove,
+    arrayUnion,
+    doc,
+    updateDoc,
+    getDoc,
+  } from 'firebase/firestore'
+
+  async function toggleLike() {
+    if (!uid) {
+      return
+    }
+
+    const storiesRef = doc(db, `stories`, storyId)
+    const storiesSnapshot = await getDoc(storiesRef)
+    const storiesData = await storiesSnapshot.data()
+    const liveLikes = storiesData?.likes
+
+    if (liveLikes?.includes(uid)) {
+      liked = false
+      likedCount--
+      return await updateDoc(storiesRef, {
+        likes: arrayRemove(uid),
+      })
+    }
+    liked = true
+    likedCount++
+    await updateDoc(storiesRef, {
+      likes: arrayUnion(uid),
+    })
+  }
+
+  $: liked = uid && likes?.includes(uid)
+  $: likedCount = likes?.length
+</script>
+
+<button on:click={toggleLike} class="btn">
+  {#if liked}
+    <img src="/heart_filled.png" alt="heart_filled" height="20" width="20" />
+  {:else}
+    <img src="/heart_outline.png" alt="heart_outline" height="20" width="20" />
+  {/if}
+  <span class="text">{likedCount}</span>
+</button>
