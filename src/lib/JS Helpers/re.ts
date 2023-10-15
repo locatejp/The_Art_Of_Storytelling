d6b49c7013f783a8d6fcaf70acb3d16c822ef8a9
JP Carlin <jpcarlin@jps-macbook-air.mynetworksettings.com>
2023-10-14 21:37:18 -0400

    Set up the mechanism to add sentences to a story

---
+++ src/lib/JS Helpers/re.ts
@@ -0,0 +1,2 @@
+export const sentenceRe = /^(?:[A-Z'"].{8,98}[.!?])(['"])?$/
+export const titleRe = /^[A-Z'"'].{3,24}/
--- src/routes/stories/[storyId]/+page.svelte
+++ src/routes/stories/[storyId]/+page.svelte
@@ -1,35 +1,46 @@
 <script lang="ts">
   import type { PageData } from './$types'
   import { page } from '$app/stores'
-  import { user } from '$lib/firebase'
+  import { user, userData } from '$lib/firebase'
   import StoryItem from '$lib/components/StoryItem.svelte'
   import Likes from '$lib/components/Likes.svelte'
+  import StoryCloser from '$lib/components/StoryCloser.svelte'
 
-  const storyId = $page.params.storyId
   export let data: PageData
-  let likes: string[] = data.likes
-  let uid = $user?.uid
+  const { storyData } = data
+  console.log({ storyData })
+  console.log({ $storyData })
+  $: photoURL = $userData?.photoURL
+  const storyId = $page.params.storyId
+  // let likes: string[] = data.likes
+  $: likes = $storyData?.likes
+  $: uid = $user?.uid
+  $: lastPostUID = $storyData?.story?.at(-1)?.uid
+  let rerenderStory = false
 </script>
 
-<main class="card w-4/6 bg-base-200 mx-auto shadow-xl">
+<main class="card w-5/6 bg-base-200 mx-auto shadow-xl">
   <div class="card-body items-center text-center">
     <h1 class="card-title text-5xl font-bold p-5">
-      {data.storyTitle}
+      {$storyData?.storyTitle}
     </h1>
     <figure>
       <img
         class="rounded-t-3xl w-3/5"
-        src={data.storyImgURL}
+        src={$storyData?.storyImgURL}
         alt="StoryImage"
         width="800"
         height="800"
       />
     </figure>
     <Likes {likes} {uid} {storyId} />
-    <ul class="list-none p-0 flex flex-col w-full">
-      {#each data.story as entry}
-        <StoryItem {entry} />
-      {/each}
-    </ul>
+    {#if $storyData}
+      <ul class="list-none p-0 flex flex-col w-full">
+        {#each $storyData?.story as entry}
+          <StoryItem {entry} />
+        {/each}
+        <StoryCloser {lastPostUID} {photoURL} {storyId} {rerenderStory} />
+      </ul>
+    {/if}
   </div>
 </main>
