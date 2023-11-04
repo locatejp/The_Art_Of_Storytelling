e338657b3e41ebc8b3149dec510f3348ed9cc1d7
JP Carlin <jpcarlin@jps-macbook-air.mynetworksettings.com>
2023-11-04 16:34:45 -0400

    Added searchbar as a component ut it doesn't do anything yet

---
+++ src/lib/components/SearchBar.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let searchValue: string
+  export let handleSearchChange: (e: any) => void
+  export let showPrimaryBorder: boolean
+</script>
+
+<input
+  name="search"
+  type="search"
+  bind:value={searchValue}
+  placeholder="Search…"
+  autocomplete="off"
+  spellcheck="true"
+  id="search"
+  class="input input-bordered w-full max-w-xs"
+  class:input-primary={showPrimaryBorder}
+  on:input={handleSearchChange}
+/>
--- src/routes/topstories/+page.svelte
+++ src/routes/topstories/+page.svelte
@@ -2,6 +2,7 @@
   import type { PageData } from './$types'
   import MyStoryLink from '$lib/components/MyStoryLink.svelte'
   import Pagination from '$lib/components/Pagination.svelte'
+  import SearchBar from '$lib/components/SearchBar.svelte'
   import { page } from '$app/stores'
 
   export let data: PageData
@@ -14,6 +15,12 @@
   $: activePage = Number($page.url?.searchParams?.get(`pageId`)) || 1
   $: setItemsArray(activePage)
 
+  $: searchValue = ``
+  function handleSearchChange(e: any) {
+    console.log({ e, searchValue, showPrimaryBorder })
+  }
+  $: showPrimaryBorder = Boolean(searchValue)
+
   let activeArr: any[] = []
   function setItemsArray(activePage: number) {
     const start = (activePage - 1) * itemsPerPage
@@ -25,6 +32,7 @@
 <main class="card w-5/6 bg-base-200 mx-auto shadow-xl">
   <div class="card-body items-center text-center">
     <h1 class="card-title text-5xl font-bold p-5">Top Stories</h1>
+    <SearchBar bind:searchValue {handleSearchChange} {showPrimaryBorder} />
     {#each activeArr as storyId}
       <MyStoryLink {storyId} />
     {/each}
